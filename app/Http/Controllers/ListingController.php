3e8040c524864da03f195da31dd61cf03c6c23ed
Unknown <unknown@unknown.com>
2023-08-15 16:08:32 +0700

    Migrations created and users displayed in index view

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -2,13 +2,22 @@
 
 namespace App\Http\Controllers;
 
+//use App\Models\User;
+
+use App\Models\User;
 use Illuminate\Http\Request;
+
 
 class ListingController extends Controller
 {
     //
-    public function index() {
+    //public function index(User $users) {
+    public function index(User $users) {
 
-        return view('listings.index');
+        return view('listings.index', [
+
+            //'users' => $users
+            'users' => User::all()
+        ]);
     }
 }
